9b0539e7236586daa9890f8f99e4d33b64c845a8
Jakub Stacherski <o503@interia.eu>
2022-04-05 23:14:10 +0200

    Working walking and collisions

--- main.cpp
+++ main.cpp
@@ -2,40 +2,42 @@
 #include <box2d/box2d.h>
 #include <memory>
 
-void move_shape(sf::Shape &shape)
+void move_body(b2Body *body)
 {
-    const float VELOCITY = 0.5f;
+    const float VELOCITY = 1.0f;
 
         // Moving objects
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
     {
-        shape.move(0.0f, -VELOCITY);
+        body->SetLinearVelocity(b2Vec2(0.0f, -VELOCITY));
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
     {
-        shape.move(0.0f, VELOCITY);
+        body->SetLinearVelocity(b2Vec2(0.0f, VELOCITY));
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
     {
-        shape.move(-VELOCITY, 0.0f);
+        body->SetLinearVelocity(b2Vec2(-VELOCITY, 0.0f));
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
     {
-        shape.move(VELOCITY, 0.0f);
+        body->SetLinearVelocity(b2Vec2(VELOCITY, 0.0f));
     }
+    if (!sf::Keyboard::isKeyPressed(sf::Keyboard::W) && !sf::Keyboard::isKeyPressed(sf::Keyboard::S) && !sf::Keyboard::isKeyPressed(sf::Keyboard::A) && !sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        body->SetLinearVelocity(b2Vec2(0.0f, 0.0f));
 }
 
 int main()
 {
     //      Box2D INIT
     // Creating world with gravity
-    b2Vec2 gravity(0.0f, -10.0f);
+    b2Vec2 gravity(0.0f, 0.0f);
     b2World world(gravity);
 
     // Creating definition for bodies
     // and setting properties
-    b2BodyDef groundBodyDef;
-    groundBodyDef.position.Set(0.0f, -10.0f);
+    b2BodyDef wallBodyDef;
+    wallBodyDef.position.Set(0.0f, -10.0f);
 
     b2BodyDef dynamicBodyDef;               // Body is static by default
     dynamicBodyDef.type = b2_dynamicBody;   // This line makes body dynamic
@@ -43,17 +45,24 @@ int main()
 
     // Create body object based on its definition
     // and addin it to the world
-    b2Body *groundBody = world.CreateBody(&groundBodyDef);
+    b2Body *wallBody = world.CreateBody(&wallBodyDef);
     b2Body *dynamicBody = world.CreateBody(&dynamicBodyDef);
 
     // Box shape
-    b2PolygonShape groundBox;
+    b2PolygonShape wallBox;
     b2PolygonShape dynamicBox;
-    groundBox.SetAsBox(50.0f, 10.0f);
-    dynamicBox.SetAsBox(1.0f, 1.0f);
+    wallBox.SetAsBox(100.0f, 10.0f);
+    dynamicBox.SetAsBox(10.0f, 10.0f);
+    wallBody->SetTransform(b2Vec2(200.0f, 200.0f), 0);
+
+    sf::RectangleShape wallShape(sf::Vector2f(100.0f, 10.0f));
+    wallShape.setFillColor(sf::Color::Red);
+    // Set initial position of wall
+    const b2Vec2& currentPosition = wallBody->GetPosition();
+    wallShape.setPosition(currentPosition.x, currentPosition.y);
 
     // Creating fixture without definition
-    groundBody->CreateFixture(&groundBox, 0.0f);
+    wallBody->CreateFixture(&wallBox, 0.0f);
 
     // Creating fixture definition to set some properties
     b2FixtureDef dynamicFixture;
@@ -70,19 +79,16 @@ int main()
     int32 velocityIterations = 6;   // Velocity calculations during one step
     int32 positionIterations = 2;   // Position calculations during one step
 
-    // for (int32 i = 0; i < 60; ++i)
-    // {
-    //     world.Step(timeStep, velocityIterations, positionIterations);
-    //     b2Vec2 position = dynamicBody->GetPosition();
-    //     float angle = dynamicBody->GetAngle();
-    //     printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
-    // }
-
     //      SFML
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-    shape.setPosition(500.0f, 500.0f);
+    sf::RectangleShape playerShape(sf::Vector2f(15.0f, 15.0f));
+    playerShape.setFillColor(sf::Color::Green);
+    playerShape.setPosition(500.0f, 500.0f);
+
+
+
+
+
 
     // Main loop
     while (window.isOpen())
@@ -95,16 +101,22 @@ int main()
                 window.close();
         }
 
-        move_shape(shape);
+        // Box2D step
+        world.Step(timeStep, velocityIterations, positionIterations);
+        move_body(dynamicBody);
+
+        sf::Vector2f origin = playerShape.getOrigin();
+        // dynamicBody->ApplyForceToCenter(b2Vec2(1.0f, 0.0f));
+
+        const b2Vec2& currentPosition = dynamicBody->GetPosition();
+        playerShape.setPosition(currentPosition.x, currentPosition.y);
 
         // Drawing on screen
         window.clear();
-        window.draw(shape);
+        window.draw(playerShape);
+        window.draw(wallShape);
         window.display();
     }
-
-
-
 
     return 0;
 }
