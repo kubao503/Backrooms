11b8b0a61c683d33128d7092b1c27fc96f6b2cf7
Jakub Stacherski <o503@interia.eu>
2022-04-05 13:17:25 +0200

    Successful installation of SFML and Box2D libraries

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ compile:
 	g++ -I src/include -c main.cpp
 
 link:
-	g++ main.o -o main -L src/lib -l sfml-graphics -l sfml-window -l sfml-system
+	g++ main.o -o main -L src/lib -l sfml-graphics -l sfml-window -l sfml-system -l box2d
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include <box2d/box2d.h>
+#include <box2d/b2_world.h>
 
 int main()
 {
+    // SFML
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
@@ -19,6 +22,12 @@ int main()
         window.draw(shape);
         window.display();
     }
+
+    // Box2D
+    b2Vec2 gravity(0.0f, -10.0f);
+    b2World world(gravity);
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(0.0f, -10.0f);
 
     return 0;
 }
