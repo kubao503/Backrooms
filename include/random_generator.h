2220d1131cd947406447d101f016d2aa62c89227
kubao503 <kubao503@pop-os.localdomain>
2022-05-13 12:40:17 +0200

    Added random generator

---
+++ include/random_generator.h
@@ -0,0 +1,43 @@
+#ifndef RANDOM_GENERATOR
+#define RANDOM_GENERATOR
+
+#include <cstdint>
+#include <random>
+#include <cassert>
+
+class RandomGenerator
+{
+public:
+    RandomGenerator(int baseSeed)
+        : baseSeed_{baseSeed} {}
+
+    void seed(int x, int y)
+    {
+        mt.seed(chunkSeed(x, y) + baseSeed_);
+    }
+
+    bool drawLots(unsigned int chance, unsigned int max)
+    {
+        assert(chance <= max);
+
+        return (mt() % max) < chance;
+    }
+    bool drawLots(double chance, double max)
+    {
+        assert(chance <= max);
+
+        int bigNumber = mt();
+        return (bigNumber / max - static_cast<int>(bigNumber / max)) * max < chance;
+    }
+
+private:
+    int chunkSeed(int x, int y)
+    {
+        return (x & 0xFFFF) << 16 | (y & 0xFFFF);
+    }
+
+    std::mt19937 mt;
+    const int baseSeed_;
+};
+
+#endif
